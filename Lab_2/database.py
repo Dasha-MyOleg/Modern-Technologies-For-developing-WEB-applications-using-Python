f697fcaf9d7932f393ae59468abf05ba4e81f632
Dasha-MyOleg <dasharikong@gmail.com>
2025-06-14 01:55:44 +0300

    real start

--- Lab_2/database.py
+++ Lab_2/database.py
@@ -5,10 +5,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # URL бази даних SQLite
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:2684@localhost:5432/dragoncollect"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./figurines.db"
 
 # Створення рушія бази даних
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 # Створення сесії для взаємодії з базою даних
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
